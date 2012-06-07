c0efe7accefd0b6eeca8f356b2498000a015d5b8
Severin Neumann <severin.neumann@altmuehlnet.de>
2012-06-07 20:25:21 +0200

    Added missing files for make check

---
+++ gb/README.txt
@@ -0,0 +1,3 @@
+Here you find precomputed groebner bases over F_{32003} using degree reverse
+lexicographic term ordering (computed using ApCoCoA). They can be used to
+check the results of parallelGBC.
---
+++ test/RunTests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# First test set, this is just a start. There will be more tests and hopefully a better testing system...
+TESTS="cyclic4 cyclic5 katsura7"
+P=${0%/*}; # Binary should be in the same path at this file
+INPUT="input/"
+COMPARE="gb/"
+for TEST in ${TESTS}; 
+do
+for PROC in `seq 1 4`; do
+R="`${P}/test-f4.bin ${INPUT}/${TEST}.txt ${PROC} 0 1 | diff - ${COMPARE}/${TEST}.txt`"
+if [ ${R} ]; then
+	echo -e "$TEST ($PROC processors)\t\t\t\t\e[01;31mfailed\e[00;30m"
+else
+	echo -e "$TEST ($PROC processors)\t\t\t\t\e[01;32mpassed\e[00;30m"
+fi;
+done
+done

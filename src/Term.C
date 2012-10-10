ad2fae27f2da8823321c6f1a5a4db4ff12edf353
Severin Neumann <severin.neumann@uni-passau.de>
2012-10-10 12:01:45 +0200

    Bugfix in F4Algorithm.C

--- src/Term.C
+++ src/Term.C
@@ -14,36 +14,47 @@
  *  You should have received a copy of the GNU General Public License
  *  along with parallelGBC.  If not, see <http://www.gnu.org/licenses/>.
 */
+#include <sstream>
 #include "../include/Term.H"
 #include "../include/Polynomial.H"
 #include "../include/F4Utils.H"
 
-std::ostream& operator<< (std::ostream &out, const Term& term)
-{ 
-	if(term.deg() > 0)
+
+
+std::string TermInstance::str() const {
+	
+	std::stringstream stream;
+
+	if(deg() > 0)
 	{
 		bool first = true;
-		for(size_t i = 0; i < term.size(); i++)
+		for(size_t i = 0; i < size(); i++)
 		{ 
-			if(term[i] > 0)
+			if(indets[i] > 0)
 			{ 
 				if(!first)
 				{ 
-					out << "*";
+					stream << "*";
 				}
 				first = false;
-				out << "x[" << (i+1) << "]";
-				if(term[i] > 1)
+				stream << "x[" << (i+1) << "]";
+				if(indets[i] > 1)
 				{
-					out << "^" << term[i];
+					stream << "^" << indets[i];
 				}
 			}
 		}
 	} else {
-		out << 1;
+		stream << 1;
 	}
-	return out;
+	return stream.str();
 } 
+
+std::ostream& operator<< (std::ostream &out, const Term& term)
+{ 
+	out << term.str();
+	return out;
+}
 
 bool TermInstance::isDivisibleBy(const TermInstance* other) const {
 	if(other == this) { return true; }
